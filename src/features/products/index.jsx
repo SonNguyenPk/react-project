9c60a1c2c799f94bb4f19b5bf103dbb255451e80
Sonnguyen <sonnguyen.pk1301@gmail.com>
2021-05-07 06:16:33 +0700

    configure jsonfig.json

--- src/features/products/index.jsx
+++ src/features/products/index.jsx
@@ -2,10 +2,11 @@ import { Container } from '@material-ui/core';
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import PageNotFound from 'src/components/PageNotFound';
+
+import { router } from 'src/utilise/routerLink';
 import AddEditProduct from './pages/AddEditProduct';
 import ProductDetail from './pages/DetailProduct';
 import ProductsData from './pages/productList';
-import { router } from 'src/utilise/routerLink';
 
 ProductFeature.propTypes = {};
 
